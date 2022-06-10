fb53a72efbd55b848095616e5ad3bee3b93e36d6
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-10 11:13:47 -0700

    complete first test

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 // Business logic
 
-function beepBoop(numbers) {}
+function beepBoop(number) {
+  let countDownArray = [];
+  for (let i = number; i >= 0; i--) {
+    countDownArray.push(i);
+  }
+  return countDownArray.reverse();
+}
+
+console.log(countDownArray(5));
+
+// let digitsArray = number.toString().split("")
+//   digitsArray.forEach(function)
