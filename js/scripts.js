01befc13f4bbf03647eaf5f39e7a2126905fb24a
weijwang18@gmail.com <weijwang18@gmail.com>
2022-06-11 15:22:44 -0700

    modify css

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,13 @@ function beepBoop(number) {
     countDownArray.push(parseInt(i));
   }
   let outputArray = countDownArray.reverse().toString().split(",");
-  // split numbers into digits
   outputArray.forEach(function (digit) {
-    if (digit.includes(3) && digit > 0) {
+    debugger;
+    if (digit.includes("3") && digit > 0) {
       outputArray[digit] = "Won't you be my neighbor?";
-    } else if (digit.includes(2) && digit > 0) {
+    } else if (digit.includes("2") && digit > 0) {
       outputArray[digit] = "Boop!";
-    } else if (digit.includes(1) && digit > 0) {
+    } else if (digit.includes("1") && digit > 0) {
       outputArray[digit] = "Beep!";
     }
   });
@@ -26,6 +26,7 @@ $(document).ready(function () {
     const answer = $("input#answer").val();
     if (answer >= 0) {
       $("#result").text(beepBoop(answer));
+      $(".card").show();
     } else {
       alert("Please enter a positive number!");
     }
